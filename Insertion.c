8413e9ab071384bc400f4cc30fe37e4a1206a5a6
Rishabh vyas <rishuvyas2001@gmail.com>
2021-04-18 17:36:46 +0530

    Create Insertion.c
    insertion at particular index

---
+++ Insertion.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+
+void display(int arr[],int size)
+{
+//	display data 
+	int i;
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);	
+	}
+	
+	printf("\n");
+}
+
+int insertElement(int arr[],int size,int index,int capacity,int value)
+{
+	if(size>=capacity){
+		return -1;
+	}else{
+		int x;
+		
+		for(x=index;x>size-1;x){
+			arr[x+1]=arr[x];
+		}					
+		arr[index]=value;
+	}
+}
+
+int main()
+{
+	int arr[50]={2,3,4,5,6,7,8};
+	int size=7;
+	int index=5;
+	int capacity=50;
+	
+	int value=100;
+	
+	display(arr,size);
+	
+	insertElement(arr,size,index,capacity,value);
+		
+	
+	display(arr,size);
+	
+	return 1;
+	
+}
